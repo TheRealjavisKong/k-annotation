11a9aceb7c1c63d5b45c3be12c645e6783b4e09e
20223520 <kong.lingyu@huafeng.com>
2022-09-15 10:52:18 +0800

    update new version 1.0.1 : optimize cost time calculation logic and add explanation source

--- src/main/java/com/github/kly/annotation/KLoger.java
+++ src/main/java/com/github/kly/annotation/KLoger.java
@@ -2,7 +2,10 @@ package com.github.kly.annotation;
 
 import java.lang.annotation.*;
 
-
+/*
+ * @author: Lingyu Kong
+ * @description: 自定义注解接口类，@Target表明该注解生效的位置是在方法，@Rentention注解表明该注解接口生效周期是RUNTIME
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- src/main/java/com/github/kly/annotation/KLogerAspect.java
+++ src/main/java/com/github/kly/annotation/KLogerAspect.java
@@ -9,6 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * @author: Lingyu Kong
+ * @description: 定义切面类，根据注解@KLoger位置，使用joinPoint动态获取被注解标识的方法名，并传入log日志中
+ * 日志包括: 进入方法，离开方法，方法耗时，异常类型和信息
+ */
 
 @Configuration
 @Aspect
@@ -19,24 +24,35 @@ public class KLogerAspect {
     public void kLogerAspect(){}
 
 
+    /*
+     * @description:
+       * @Param joinPoint: 用于控制被注解方法的运行和动态获取方法名，参数等
+       * @return: 返回的result是被注解方法的返回值，如果不返回result会造成被注解方法无返回值
+     */
     @Around("kLogerAspect()")
-    public Object AroundLogPointCut(ProceedingJoinPoint joinPoint) {
+    public Object AroundLogPointCut(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
-        long time =  System.currentTimeMillis() - startTime;
-        Object result = recordLog(joinPoint,time);
+        Object result = recordLog(joinPoint,startTime);
         return result;
     }
 
-    private Object recordLog(ProceedingJoinPoint joinPoint, Long time) {
+    /*
+     * @description:
+       * @Param joinPoint: 使用result = joinPoint.proceed()表达式即可控制被注解的方法运行，并获取方法运行的返回值
+       * @Param startTime: 用于计算运行时间
+       * @return: 被注解方法的运行结果
+     */
+    private Object recordLog(ProceedingJoinPoint joinPoint, Long startTime) throws Throwable {
         String methodName = joinPoint.getSignature().getName();
         Object result =null;
         try{
             logger.info("====================================================");
             logger.info("ENTER METHOD:{}",methodName);
             result = joinPoint.proceed();
-        }catch (Throwable e){
-            logger.error(e.getMessage());
+        }catch (Exception e){
+            logger.error("ENCOUNTER EXCEPTION:{}, MESSAGE:{} ",e.getClass().getName(), e.getMessage());
         }
+        long time =  System.currentTimeMillis() - startTime;
         logger.info("LEAVE METHOD:{}, COST TIME:{} ms",methodName,time);
         logger.info("====================================================");
         return result;
